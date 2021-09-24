f6617de5802827828ac1176ba89d14353970a2ce
Ezckal <91284130+Ezckal@users.noreply.github.com>
2021-09-24 10:48:40 -0700

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,58 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+.......ffffff.......
+.....fff3333fff.....
+....ff33333331ff....
+...ff33333333333f...
+...f333333333333f...
+..ff3333313313333f..
+.ff3333333f33f333f..
+.f33333333f33f333ff.
+f333333223f33f23333f
+f333333333333333333f
+f333333333332333333f
+f333333333333333333f
+.ff333333333333333f.
+..f22233333333322f..
+.f2122233333322212f.
+f222222233332222222f
+f22222222ff22222222f
+.ffffffff..ffffffff.
+`;
+            case "image2":
+            case "myImage0":return img`
+. . 2 f . f f f f . f 2 . . . . 
+. . . . f f f f f f . . . . . . 
+2 f f f f f 3 3 f f f f f . . . 
+. . f 2 f f f f f f 2 f . . . . 
+. f . . . . . . . . . . . . . . 
+f . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
